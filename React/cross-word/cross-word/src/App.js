64830a457de2a04250acf09d88bbf4ee43844d15
seunghwan <asdryzx@naver.com>
2023-03-27 17:58:36 +0900

    ㅅ줭

--- React/cross-word/cross-word/src/App.js
+++ React/cross-word/cross-word/src/App.js
@@ -142,6 +142,7 @@ function App() {
     answers: [...Array(4)].map((e) => Array(4).fill("")),
     dir: null,
   });
+  const hightRef = useRef([]);
 
   const findClue = useCallback(
     (clueNum, tab) => {
@@ -447,7 +448,7 @@ function App() {
   );
 
   useEffect(() => {
-    console.log("재렌더링!");
+    console.log("useEffect!");
     document.addEventListener("keydown", keyPressHandler);
     return () => {
       document.removeEventListener("keydown", keyPressHandler);
@@ -466,10 +467,11 @@ function App() {
       } else {
         copy[index + i * 4] = {
           ...copy[index + i * 4],
-          hightlight: !copy[i + index].hightlight,
+          hightlight: !copy[index + i * 4].hightlight,
         };
       }
     }
+    console.log("변경함!");
     setWordList(copy);
   };
 
@@ -514,10 +516,11 @@ function App() {
         {clueList.map((clue, idx) => {
           return (
             <li
+              style={{ marginBottom: "5vh" }}
               key={idx}
               data-clue={clue.clue}
-              // onMouseOver={() => toggleClue(clue)}
-              // onMouseOut={() => toggleClue(clue)}
+              onMouseOver={() => toggleClue(clue)}
+              onMouseOut={() => toggleClue(clue)}
             >
               {idx + ".  " + clue.content}
             </li>
