56f65408b9af64dcc00511eef56eaf65847dc280
seunghwanYun <asdryzx2@gmail.com>
2023-07-10 17:10:15 +0900

    가변메서드

---
+++ Swift/syntax/가변 메서드.playground/Contents.swift
@@ -0,0 +1,23 @@
+protocol Resettable {
+    mutating func reset()
+}
+
+class Person: Resettable {
+    var name: String?
+    var age: Int?
+    
+    func reset() {
+        self.name = nil
+        self.age = nil
+    }
+}
+
+struct Point: Resettable {
+    var x: Int = 0
+    var y: Int = 0
+    
+    mutating func reset() {
+        self.x = 0
+        self.y = 0
+    }
+}
