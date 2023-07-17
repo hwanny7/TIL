8de21862285808ffa15692f293cc998c51784593
seunghwanYun <asdryzx2@gmail.com>
2023-07-17 17:11:42 +0900

    observable 패턴 클래스 생성

---
+++ project/client/client/Application/AppConfigurations.swift
@@ -0,0 +1,8 @@
+//
+//  AppConfigurations.swift
+//  client
+//
+//  Created by yun on 2023/07/17.
+//
+
+import Foundation
---
+++ project/client/client/Presentation/Utils/Protocols/Observable.swift
@@ -0,0 +1,8 @@
+//
+//  Observable.swift
+//  client
+//
+//  Created by yun on 2023/07/17.
+//
+
+import Foundation
